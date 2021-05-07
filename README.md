157a1d22cbfe29d453b7d7463c165247583668f1
M. Zulfikar Isnaen <isnaen70@gmail.com>
2021-05-07 16:43:55 +0700

    get data City using JSON

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # json-api
+
+npm install @google/clasp -g
+clasp login
+clasp clone <ID> --rootDir src
+npm i --save @types/google-apps-script
+
+setting Tampilkan file manifes "appsscript.json" di editor di google editor
+https://script.google.com/home/usersettings aktifkan ini
+clasp push
--- src/Kode.js
+++ src/Kode.js
@@ -1,7 +1,22 @@
 function doGet() {
-  const response = [{status: "ok!"}];
+
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const ws = ss.getSheetByName("City");
+  const data = ws.getRange("A1").getDataRegion().getValues();
+  const headers = data.shift();
+
+  const jsonArray = data.map(r => {
+    let obj = {};
+    headers.forEach((h,i) => {
+      obj[h] = r[i];
+    });
+    return obj;
+  });
+
+  const response = [{status: 200, data: jsonArray}];
   return ContentService
-    .createTextOutput(JSON.stringify(response)).setMimeType(ContentService.MimeType.JSON);
+    .createTextOutput(JSON.stringify(response))
+    .setMimeType(ContentService.MimeType.JSON);
 }
 
 
