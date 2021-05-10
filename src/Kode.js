1e68666779cd3f62d0efe2c390d7e666ac5e33f5
M. Zulfikar Isnaen <isnaen70@gmail.com>
2021-05-10 14:30:00 +0700

    add method delete using post

--- src/Kode.js
+++ src/Kode.js
@@ -49,3 +49,26 @@ function sendJSON_(jsonResponse){
     .createTextOutput(JSON.stringify(jsonResponse))
     .setMimeType(ContentService.MimeType.JSON);
 }
+
+function doPost(e){
+  let flag = false;
+  const ss = SpreadsheetApp.getActiveSpreadsheet(); //get active spreadsheet
+  const ws = ss.getSheetByName("AssemblyLine"); //get sheet
+  let IdAssembly = e.parameter.IdAssembly;
+  let lastRow = ws.getLastRow();
+
+  for (var row = 1; row < lastRow; row++){
+    let IdAssemblyServer = ws.getRange(row, 1).getValue();
+    if (IdAssembly==IdAssemblyServer){
+      ws.deleteRow(row);
+      flag = true;
+    }
+  }
+  if (flag==0){
+    let result = "Id not found!";
+  } else {
+    let result = "Success delete";
+  }
+  result = JSON.stringify({"result": result});
+  return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.JSON);
+}
