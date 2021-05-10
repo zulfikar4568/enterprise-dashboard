1b1637a2071bb61aee6ea60a5ac3651116010089
M. Zulfikar Isnaen <isnaen70@gmail.com>
2021-05-10 14:32:22 +0700

    add delete method

--- src/Kode.js
+++ src/Kode.js
@@ -1,7 +1,7 @@
 function doGet() {
 
   const ss = SpreadsheetApp.getActiveSpreadsheet(); //get active spreadsheet
-  const ws = ss.getSheetByName("City"); //get sheet
+  const ws = ss.getSheetByName("Plant"); //get sheet
   const data = ws.getRange("A1").getDataRegion().getValues(); //get data from A1
   const headers = data.shift(); //get data headers
 
@@ -19,71 +19,25 @@ function doGet() {
 }
 
 
-function doPost(e) {
-  /*
-    {
-      IdCity: 1,
-      NameCity: "Jakarta",
-      Description: "Location 1",
-      Capacity: 0,
-      OEE: 0,
-      Output: 0,
-      Status: "Red"
-    }
-  */
- 
-  let jsonResponse;
-
+function doPost(e){
+  let flag = false;
   const ss = SpreadsheetApp.getActiveSpreadsheet(); //get active spreadsheet
-  const ws = ss.getSheetByName("City"); //get sheet
+  const ws = ss.getSheetByName("Plant"); //get sheet
+  let IdAssembly = e.parameter.IdAssembly;
+  let lastRow = ws.getLastRow();
 
-  const headers = ws.getRange(1,1,1,ws.getLastColumn()).getValues()[0]; //getRange(1row, 1column, number row, number column).get first row;
-  const headerOriginalOrder = headers.splice();
-  headerOriginalOrder.shift();
-
-  // remove id columns header
-  headers.shift();
-  headers.sort();
-  //All above is to get from template ^^^^
-
-  // Here is from event
-  const body = e.postData.contents;
-  const bodyJSON = JSON.parse(body);
-  const headersPassed = Object.keys(bodyJSON).sort();
-
-  if (!compareTwoArray_(headers, headersPassed)){
-    jsonResponse = {status: 500, message: "Invalid Argument Passed!"};
-    return sendJSON_(jsonResponse);
+  for (var row = 1; row < lastRow; row++){
+    let IdAssemblyServer = ws.getRange(row, 1).getValue();
+    if (IdAssembly==IdAssemblyServer){
+      ws.deleteRow(row);
+      flag = true;
+    }
   }
-
-  const arrayOfData = headerOriginalOrder.map(h => bodyJSON[h]);
-
-  const aoaIds = ws.getRange(2,1,ws,getLastRow()-1,1).getValues();
-  const newIDNumber = getMaxFromArrayOfArray_(aoaIds) + 1;
-  arrayOfData.unshift(newIDNumber);
-  ws.appendRow([arrayOfData]);
-}
-
-//return true if  all item are the same
-function compareTwoArray_(arr1, arr2){
-  if (arr1.length !== arr2.length) return false;
-  for(let i=0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]) return false;
+  if (flag==0){
+    let result = "Id not found!";
+  } else {
+    let result = "Success delete";
   }
-  return true;
-}
-
-function sendJSON_(jsonResponse){
-  return ContentService
-    .createTextOutput(JSON.stringify(jsonResponse))
-    .setMimeType(ContentService.MimeType.JSON);
-}
-
-//return highest id
-function getMaxFromArrayOfArray_(aoa){
-  let maxID = 0;
-  aoa.forEach(r => {
-    if(r[0] > maxID) maxID = r[0];
-  });
-  return maxID;
+  result = JSON.stringify({"result": result});
+  return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.JSON);
 }
