898d515dbf8e3edffd5ce24435cc42c42392aa3d
M. Zulfikar Isnaen <isnaen70@gmail.com>
2021-05-11 15:10:38 +0700

    method update assembly line

--- src/Kode.js
+++ src/Kode.js
@@ -52,9 +52,70 @@ function sendJSON_(jsonResponse){
 }
 
 function doPost(e){
-  let flag = false;
   const ss = SpreadsheetApp.getActiveSpreadsheet(); //get active spreadsheet
   const ws = ss.getSheetByName("AssemblyLine"); //get sheet
+  
+  switch(e.parameter.Action){
+    case "delete":
+      return doDelete(e, ws);
+      break;
+    case "update":
+      return doUpdate(e, ws);
+      break;
+  }
+}
+
+function doUpdate(e, ws){
+  let dataArray = [];
+  dataArray.push(e.parameter.LineNo);
+  dataArray.push(e.parameter.Description);
+  dataArray.push(e.parameter.PersonInCharge);
+  dataArray.push(e.parameter.PICPhone);
+  dataArray.push(e.parameter.PICEmail);
+  dataArray.push(e.parameter.OEE);
+  dataArray.push(e.parameter.Capacity);
+  dataArray.push(e.parameter.Output);
+  dataArray.push(e.parameter.Status);
+  dataArray.push(e.parameter.Performance);
+  dataArray.push(e.parameter.Availability);
+  dataArray.push(e.parameter.Quality);
+  dataArray.push(e.parameter.Plant);
+  dataArray.push(e.parameter.NCRCauseList);
+  dataArray.push(e.parameter.NCRCauseList_2);
+  dataArray.push(e.parameter.NCRCauseList_3);
+  dataArray.push(e.parameter.NCRCauseList_4);
+  dataArray.push(e.parameter.NCRCauseList_5);
+  let IdAssemblyLine = e.parameter.IdAssemblyLine;
+
+  let flag = 0;
+  let lastRow = ws.getLastRow();
+  const headers = ws.getRange("A1").getDataRegion().getValues().shift();
+
+  for (var row = 1; row < lastRow; row++){
+    let IdAssemblyLineServer = ws.getRange(row, 1).getValue();
+    if (IdAssemblyLine==IdAssemblyLineServer){
+      for(var column=1; column<=headers.length;column++){
+        if (column>1){
+          ws.getRange(row, column).setValue(dataArray[column-2]);
+        }
+      }
+      flag = true;
+    }
+  }
+  let message, status;
+  if (flag==false){
+    status = 404
+    message = "Id is not found!";
+  } else {
+    status = 200
+    message = "Success update the data";
+  }
+  message = JSON.stringify({"status": status, "message": message});
+  return ContentService.createTextOutput(message).setMimeType(ContentService.MimeType.JSON);
+}
+
+function doDelete(e, ws){
+  let flag = false;
   let IdAssemblyLine = e.parameter.IdAssemblyLine;
   let lastRow = ws.getLastRow();
 
