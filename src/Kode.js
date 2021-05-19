db69aeee5af660b3d643f8ff437c2e7dff76ba0e
M. Zulfikar Isnaen <isnaen70@gmail.com>
2021-05-19 16:36:22 +0700

    treshold added

--- src/Kode.js
+++ src/Kode.js
@@ -1,7 +1,7 @@
 function doGet() {
 
   const ss = SpreadsheetApp.getActiveSpreadsheet(); //get active spreadsheet
-  const ws = ss.getSheetByName("NCRCauseList"); //get sheet
+  const ws = ss.getSheetByName("Treshold"); //get sheet
   const data = ws.getRange("A1").getDataRegion().getValues(); //get data from A1
   const headers = data.shift(); //get data headers
 
@@ -26,7 +26,7 @@ function sendJSON_(jsonResponse){
 
 function doPost(e){
   const ss = SpreadsheetApp.getActiveSpreadsheet(); //get active spreadsheet
-  const ws = ss.getSheetByName("NCRCauseList"); //get sheet
+  const ws = ss.getSheetByName("Treshold"); //get sheet
   
   switch(e.parameter.Action){
     case "delete":
@@ -40,19 +40,23 @@ function doPost(e){
 
 function doUpdate(e, ws){
   let dataArray = [];
-  dataArray.push(e.parameter.Value);
-  dataArray.push(e.parameter.CauseName);
-  dataArray.push(e.parameter.CauseSymptom);
-  dataArray.push(e.parameter.AffectedPart);
-  let IdNCRCauseList = e.parameter.IdNCRCauseList;
+  dataArray.push(e.parameter.RedYellowAvailability);
+  dataArray.push(e.parameter.RedYellowPerformance);
+  dataArray.push(e.parameter.RedYellowQuality);
+  dataArray.push(e.parameter.RedYellowDefault);
+  dataArray.push(e.parameter.YellowGreenAvailability);
+  dataArray.push(e.parameter.YellowGreenPerformance);
+  dataArray.push(e.parameter.YellowGreenQuality);
+  dataArray.push(e.parameter.YellowGreenDefault);
+  let IdTreshold = e.parameter.IdTreshold;
 
   let flag = 0;
   let lastRow = ws.getLastRow();
   const headers = ws.getRange("A1").getDataRegion().getValues().shift();
 
   for (var row = 1; row < lastRow; row++){
-    let IdNCRCauseListServer = ws.getRange(row, 1).getValue();
-    if (IdNCRCauseList==IdNCRCauseListServer){
+    let IdTresholdServer = ws.getRange(row, 1).getValue();
+    if (IdTreshold==IdTresholdServer){
       for(var column=1; column<=headers.length;column++){
         if (column>1){
           ws.getRange(row, column).setValue(dataArray[column-2]);
@@ -75,12 +79,12 @@ function doUpdate(e, ws){
 
 function doDelete(e, ws){
   let flag = false;
-  let IdNCRCauseList = e.parameter.IdNCRCauseList;
+  let IdTreshold = e.parameter.IdTreshold;
   let lastRow = ws.getLastRow();
 
   for (var row = 1; row < lastRow; row++){
-    let IdNCRCauseListServer = ws.getRange(row, 1).getValue();
-    if (IdNCRCauseList==IdNCRCauseListServer){
+    let IdTresholdServer = ws.getRange(row, 1).getValue();
+    if (IdTreshold==IdTresholdServer){
       ws.deleteRow(row);
       flag = true;
     }
